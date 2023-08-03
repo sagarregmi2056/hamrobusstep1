c3f3db145a256c94b78019c915ec57dba0823ef8
sagar <sagarregmi2056@gmail.com>
2023-08-03 16:21:59 +0545

    added schema design at readme file

---
+++ models/helpers/dbConnection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+
+    // The function starts by creating a reference to module.exports and stores it in the variable self. This is done to enable a retry mechanism in case the database connection fails initially.
+    const self = module.exports;
+    return mongoose
+        .connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+        })
+        .then(() => console.log("DB Connected"))
+        .catch(err => {
+            console.error(
+                "Failed to connect to mongo on startup - retrying in 5 sec", err
+            );
+            setTimeout(self, 5000);
+        });
+};
+
+// This retry mechanism allows the application to periodically attempt to reconnect to the database if the initial connection fails, which might be useful in scenarios where the database server is not immediately available or when the connection gets interrupted for some reason.
