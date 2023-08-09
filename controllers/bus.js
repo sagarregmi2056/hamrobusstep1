4451df7f42612f395a0f706f0067970b0fe7c76f
sagar <sagarregmi2056@gmail.com>
2023-08-09 10:51:04 +0545

    add more function on bus controller

--- controllers/bus.js
+++ controllers/bus.js
@@ -69,3 +69,121 @@ exports.getBuses = async (req, res) => {
   
     res.json(buses);
   };
+
+
+
+
+//   aba aru same xa 
+
+
+exports.getAvailableBusesOfOwner = async (req, res) => {
+    const buses = await Bus.find({ owner: req.ownerauth, isAvailable: true })
+      .populate("owner", "name")
+      .populate("travel", "name")
+      .sort({ created: -1 });
+  
+    res.json(buses);
+  };
+
+
+
+
+  exports.getUnavailableBusesOfOwner = async (req, res) => {
+    const buses = await Bus.find({ owner: req.ownerauth, isAvailable: false })
+      .populate("owner", "name")
+      .populate("travel", "name")
+      .sort({ created: -1 });
+  
+    res.json(buses);
+  };
+
+  
+
+//   home page ko lagi search wala
+
+exports.searchBus = async (req, res) => {
+    if (_.size(req.query) < 1)
+      return res.status(400).json({ error: "Invalid query" });
+  
+    const { startLocation, endLocation, journeyDate } = req.query;
+  
+    const bus = await Bus.find({
+      startLocation,
+      endLocation,
+      journeyDate,
+      isAvailable: true
+    })
+      .populate("travel", "name")
+      .populate("startLocation", "name")
+      .populate("endLocation", "name");
+  
+    return res.json(bus);
+  };
+
+
+
+//   startLocation: The starting location of the bus route.
+// endLocation: The destination of the bus route.
+// journeyDate: The date of the journey.
+// travel: An array of travel options (possibly representing different bus services or companies).
+// type: An array of bus types (e.g., regular, luxury, etc.).
+
+
+exports.searchBusByFilter = async (req, res) => {
+    const { startLocation, endLocation, journeyDate, travel, type } = req.body;
+    const bus = await Bus.find({
+      startLocation,
+      endLocation,
+      journeyDate,
+      isAvailable: true,
+      travel: { $in: travel },
+      type: { $in: type }
+    })
+      .populate("travel", "name")
+      .populate("startLocation", "name")
+      .populate("endLocation", "name");
+    res.json(bus);
+  };
+
+
+
+
+  exports.create = async (req, res) => {
+    const busExists = await Bus.findOne({ busNumber: req.body.busNumber });
+    if (busExists)
+      return res.status(403).json({
+        error: "Bus is already added!"
+      });
+  
+    if (req.file !== undefined) {
+      const { filename: image } = req.file;
+  
+      //Compress image
+      await sharp(req.file.path)
+        .resize(800)
+        .jpeg({ quality: 100 })
+        .toFile(path.resolve(req.file.destination, "resized", image));
+      fs.unlinkSync(req.file.path);
+      req.body.image = "busimage/resized/" + image;
+    }
+
+    if (req.body.boardingPoints) {
+        req.body.boardingPoints = req.body.boardingPoints.split(",");
+      }
+      if (req.body.droppingPoints) {
+        req.body.droppingPoints = req.body.droppingPoints.split(",");
+      }
+    
+      const bus = new Bus(req.body);
+      bus.seatsAvailable = req.body.numberOfSeats
+    
+      if (!checkDateAvailability(req.body.journeyDate)) {
+        bus.isAvailable = false;
+      }
+    
+      bus.owner = req.ownerauth;
+    
+      await bus.save();
+    
+      res.json(bus);
+    };    
