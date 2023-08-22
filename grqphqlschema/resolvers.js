539031e5c65fdee5a935db50f0df0b3ee9bef727
sagar <sagarregmi2056@gmail.com>
2023-08-22 12:29:40 +0545

    working on owner portion

--- grqphqlschema/resolvers.js
+++ grqphqlschema/resolvers.js
@@ -9,6 +9,13 @@ const resolvers = {
         const users = await models.User.find({}, 'name');
         return users.map(user => user.name);
       },
+
+      allOwners: async (_, __, { models }) => {
+        return models.Owner.find();
+      },
+      owner: async (_, { id }, { models }) => {
+        return models.Owner.findById(id);
+      },
     },
     Mutation: {
       createUser: async (_, { userInput }) => {
