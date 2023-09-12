a74b9cd7a8ffe3e07fba577f85efc941b1a0517e
sagar <sagarregmi2056@gmail.com>
2023-09-12 08:51:49 +0545

    chore/gql/contextfile

--- context.js
+++ context.js
@@ -1,25 +1,23 @@
-const User = require('./models/User');
-const Owner = require('./models/Owner');
-const Travel = require('./models/Travel')
-const Location = require('./models/Travel')
-const Bus = require('./models/Bus')
-const Booking = require('./models/Booking')
-const Guest = require("./models/Guest")
-
-
+const User = require("./models/User");
+const Owner = require("./models/Owner");
+const Travel = require("./models/Travel");
+const Location = require("./models/Travel");
+const Bus = require("./models/Bus");
+const Booking = require("./models/Booking");
+const Guest = require("./models/Guest");
 
 const context = {
-    models: {
-      User,
-      Owner,
-      Travel,
-      Location,
-      Bus,
-      Booking,
-      Guest,
-      // ... other models ...
-    },
-  };
-  
-  module.exports = context;
-  
+  models: {
+    User,
+    Owner,
+    Travel,
+    Location,
+    Bus,
+    Booking,
+    Guest,
+
+    // ... other models ...
+  },
+};
+
+module.exports = context;
