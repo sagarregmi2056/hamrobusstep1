92fb2629f30cec6584b7d7109f366aa5950b0b4e
sagar <sagarregmi2056@gmail.com>
2023-10-03 13:07:03 +0545

    admin/approve,disapprove bus

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,7 @@ exports.updateOwnerStatus = async (req, res) => {
     try {
       const { ownerId, status } = req.body;
   
-      // Check if the user making the request is a super admin
+      // Checking if the user making the request is a super admin
       if (req.ownerauth.role !== "superadmin") {
         return res.status(403).json({ error: "Permission denied" });
       }
@@ -39,6 +39,89 @@ exports.updateOwnerStatus = async (req, res) => {
       res.json({ message: "Owner status updated successfully", owner });
     } catch (err) {
       console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
+
+//   function to approve owner 
+
+  exports.approveOwner = async (req, res) => {
+    try {
+      const ownerId = req.params.ownerId;
+  
+      // Find the owner by ID
+      const owner = await Owner.findById(ownerId);
+  
+      if (!owner) {
+        return res.status(404).json({ error: "Owner not found" });
+      }
+  
+      // Update the owner's status to "approved"
+      owner.status = "approved";
+  
+      // Save the updated owner document
+      await owner.save();
+  
+      res.json({ message: "Owner approved successfully", owner });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
+  // Controller function to reject an owner without reason
+exports.rejectOwner = async (req, res) => {
+    try {
+      const ownerId = req.params.ownerId;
+  
+      // Find the owner by ID
+      const owner = await Owner.findById(ownerId);
+  
+      if (!owner) {
+        return res.status(404).json({ error: "Owner not found" });
+      }
+  
+      // Update the owner's status to "rejected"
+      owner.status = "rejected";
+  
+      // Save the updated owner document
+      await owner.save();
+  
+      res.json({ message: "Owner rejected successfully", owner });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
+//   to reject them with a reason 
+
+  exports.rejectOwner = async (req, res) => {
+    try {
+      const ownerId = req.params.ownerId;
+      const { rejectionReason } = req.body; // Extract the rejection reason from the request body
+  
+      // Find the owner by ID
+      const owner = await Owner.findById(ownerId);
+  
+      if (!owner) {
+        return res.status(404).json({ error: "Owner not found" });
+      }
+  
+      // Update the owner's status to "rejected" and save the rejection reason
+      owner.status = "rejected";
+      owner.rejectionReason = rejectionReason;
+  
+      // Save the updated owner document
+      await owner.save();
+  
+      res.json({ message: "Owner rejected successfully",reason: rejectionReason , owner });
+    } catch (error) {
+      console.error(error);
       res.status(500).json({ error: "Internal server error" });
     }
   };
--- routes/admin.js
+++ routes/admin.js
@@ -4,15 +4,39 @@ const express = require("express");
 const {
   getAllAdminOwners,
   updateOwnerStatus,
-  
+  approveOwner,
+  rejectOwner
+
 } = require("../controllers/admin.js")
 const { requireSuperadminSignin } = require("../controllers/auth-owner");
-const { getAllUsers,userById } = require("../controllers/user");
+// const { getAllUsers,userById } = require("../controllers/user");
 
 const router = express.Router();
 // recently added routes
 router.get("/allowners", requireSuperadminSignin,getAllAdminOwners );
 router.put("/owners/status", requireSuperadminSignin, updateOwnerStatus);
+
+
+
+
+
+
+
+router.put("/owners/:ownerId", requireSuperadminSignin,updateOwnerStatus);
+// Route to approve an owner
+router.put("/owners/:ownerId/approve", requireSuperadminSignin, approveOwner);
+
+// Route to rejecting  owner with a reason yaha chai reject chai hamro reason ho 
+router.put("/owners/:ownerId/reject", requireSuperadminSignin, rejectOwner);
+
+
+
+
+
+
+
+
+// old routes 
 
 // router.get("/:ownerId", requireSuperadminSignin, read);
 
