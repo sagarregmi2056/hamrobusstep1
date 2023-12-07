03f27b2bab403c66514da8eb75e89e037df592d8
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2023-12-07 15:23:10 +0545

    started implementing superadmin

--- seeds/superadmin.js
+++ seeds/superadmin.js
@@ -1,36 +1,30 @@
-// need to export owner model here 
+// need to export owner model here
 
 const Owner = require("../models/Owner");
 
-require('dotenv').config();
+require("dotenv").config();
 exports.seedSuperAdmin = async () => {
+  // yadi milxa vanya super admin ko id pw role
+  const email = process.env.SEMAIL;
+  const password = process.env.SPASSWORD;
+  const role = "superadmin";
 
+  const ownerExists = await Owner.findOne({ email });
+  if (ownerExists) return;
 
-    // yadi milxa vanya super admin ko id pw role 
-    const email =process.env.SEMAIL
-    const password =process.env.SPASSWORD
-    const role = "superadmin"
+  // yadi tyo xaina vanya new super admin khoja
+  const newowner = new Owner({
+    email,
+    password,
+    role,
+    name: "SAdmin",
+    citizenshipNumber: "980-422-8593",
+    phone: +9779804228593,
+    status: "approved",
+    vendorDetail: "success",
+    isVerified: true,
+  });
 
-
-
-    const ownerExists = await Owner.findOne({ email });
-    if (ownerExists) return;
-
-
-
-// yadi tyo xaina vanya new super admin khoja
-    const newowner = new Owner({
-        email,
-        password,
-        role,
-        name: 'SAdmin',
-        citizenshipNumber: '980-422-8593',
-        phone: 9804228593,
-        status: "approved",
-        vendorDetail:"success",
-        isVerified: true
-    });
-
-    await newowner.save();
-    console.warn("Superadmin seeded successfully...")
-  };
+  await newowner.save();
+  console.warn("Superadmin seeded successfully...");
+};
